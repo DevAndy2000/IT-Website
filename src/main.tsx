fd6889b3d343f5377974243c021d58ca59d4c9d8
DevAndy2000 <nirbheet.anand2000@gmail.com>
2025-04-27 13:28:15 -0500

    fixing site

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import 'uno.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // or any other CSS file you have
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
