bd97da764b7982746195d3349f1eeaaa2d874e4c
DevAndy2000 <nirbheet.anand2000@gmail.com>
2025-04-27 14:49:03 -0500

    Updated App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'; // or any other CSS file you have
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from 'react'
+ import ReactDOM from 'react-dom/client'
+ import App from './App'
+ import 'uno.css'
+ 
+ ReactDOM.createRoot(document.getElementById('root')!).render(
+   <React.StrictMode>
+     <App />
+   </React.StrictMode>,
+ )
